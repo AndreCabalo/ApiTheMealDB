9019b455669468f5a48e697d4d558a373173b954
Andre Cabalo <andreccabalo@gmail.com>
2024-08-03 11:03:34 -0300

    testando API

--- src/main/java/org/andrecabalo/Teste/TestandoApi.java
+++ src/main/java/org/andrecabalo/Teste/TestandoApi.java
@@ -1,2 +1,33 @@
-package org.andrecabalo.Teste;public class TestandoApi {
+package org.andrecabalo.Teste;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class TestandoApi {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome de uma receita: ");
+        String nomeReceita = scanner.nextLine();
+
+        System.out.println("Informações disponíveis para a receita " + nomeReceita + ":");
+
+        String endereco = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+        String enderecoCompleto = endereco + nomeReceita;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(enderecoCompleto))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
 }
